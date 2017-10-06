72575c680c11c33b8ddda808a529c4c9765ef21c
Daniel Eineving <daniel@eineving.se>
2017-10-06 13:46:51 +0200

    Added sample json return value

--- backend/src/main/java/WebInterface.java
+++ backend/src/main/java/WebInterface.java
@@ -1,6 +1,18 @@
+import model.DigitalPart;
+import model.Order;
+import model.OrderedPart;
+
+import java.util.List;
+
 /**
  * Created by danie on 2017-09-28.
  */
 public interface WebInterface {
-    String getAllPrintsForStl(String stlID);
+    String dummy(String text);
+    //List<Order> getOrdersFromCustomer(int customerID);
+    //List<OrderedPart> getOrderedPartsFromOrder(int orderID);
+    DigitalPart getDigitalPart(int digitalPartID);
+
+
+
 }
--- backend/src/main/java/WebInterfaceImpl.java
+++ backend/src/main/java/WebInterfaceImpl.java
@@ -1,9 +1,19 @@
+import model.DigitalPart;
+
+import java.util.List;
+
 /**
  * Created by danie on 2017-09-28.
  */
 public class WebInterfaceImpl implements WebInterface {
     @Override
-    public String getAllPrintsForStl(String stlID) {
-        return "Fuck yes! You gave me the ID " + stlID + ".";
+    public String dummy(String id) {
+        return "Fuck yes! You gave me the ID " + id + ".";
+    }
+
+    @Override
+    public DigitalPart getDigitalPart(int digitalPartID) {
+        //TODO implement this properly
+        return new DigitalPart(1337, "Stl/path/stuff.stl", null);
     }
 }
