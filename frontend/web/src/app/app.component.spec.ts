aed852a5ba19abad784c29075ed4a225d1c0f92a
Christopher <christopher.akersten@gmail.com>
2017-10-07 14:51:30 +0200

    fixed tests

--- frontend/web/src/app/app.component.spec.ts
+++ frontend/web/src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {StlViewerComponent} from "./stl-viewer/stl-viewer.component";
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        StlViewerComponent
       ],
     }).compileComponents();
   }));
@@ -18,10 +20,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('app');
   }));
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+  });
 });
