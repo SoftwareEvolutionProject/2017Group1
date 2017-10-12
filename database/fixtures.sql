9abac3e72c52829d3dc5320e160c8b2b3642fbdf
Christopher <christopher.akersten@gmail.com>
2017-10-12 20:31:26 +0200

    added fixtures

--- database/fixtures.sql
+++ database/fixtures.sql
@@ -10,4 +10,8 @@ VALUES
 
 INSERT INTO tokens(token, userid)
 values('testtoken',(select id from users where username = 'lee' LIMIT 1));
+
+INSERT INTO public.customers (id, name, email) VALUES (1, 'test', 'test');
+INSERT INTO public.customers (id, name, email) VALUES (2, 'jhon doe', 'example@example.com');
+
 COMMIT;
