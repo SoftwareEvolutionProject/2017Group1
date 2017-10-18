8b5741a66d336d5630eedcd4724cfa560615543e
Daniel Eineving <daniel@eineving.se>
2017-10-18 20:34:20 +0200

    Added proper db schema

--- database/fixtures.sql
+++ database/fixtures.sql
@@ -1,17 +1,17 @@
 BEGIN;
 
 
-INSERT INTO "user"(username, email,password)
+INSERT INTO "users"(username, email,password)
 VALUES
   ('lee','lee@yahoo.com','lee'),
   ('emma','emma@yahoo.com','emma'),
   ('cate','cate@yahoo.com','cate'),
   ('steve','steve@yahoo.com','steve');
 
-INSERT INTO token(token, userid)
-values('testtoken',(select id from "user" where username = 'lee' LIMIT 1));
+INSERT INTO tokens(token, userid)
+values('testtoken',(select id from "users" where username = 'lee' LIMIT 1));
 
-INSERT INTO public.customer (name, email) VALUES ('test', 'test');
-INSERT INTO public.customer (name, email) VALUES ('jhon doe', 'example@example.com');
+INSERT INTO customer (name, email) VALUES ('test', 'test');
+INSERT INTO customer (name, email) VALUES ('jhon doe', 'example@example.com');
 
 COMMIT;
