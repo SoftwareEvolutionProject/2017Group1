92a3a7b13959f6f1c08adf37f7c64d95e23ef71b
Jimmy Berlin <berlin.jimmy@gmail.com>
2017-11-01 22:35:40 +0100

    Adds the possibility of finding the namespace

--- js-to-ts-tool/js-to-ts-tool/Program.cs
+++ js-to-ts-tool/js-to-ts-tool/Program.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace js_to_ts_tool {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello world!");
+            if (args.Length != 1) {
+                Console.WriteLine("You have to provide a file to refractor/convert");
+                return;
+            }
+
+            string[] fileText;
+            try {
+                fileText = File.ReadAllLines(args[0]);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                return;
+            }
+
+            TransformAndRefactor tr = new TransformAndRefactor(fileText);
+            tr.run();
         }
     }
 }
---
+++ js-to-ts-tool/js-to-ts-tool/TransformAndRefactor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace js_to_ts_tool {
+    class TransformAndRefactor {
+
+        string[] fileContent;
+        string jsNamespace;
+
+        public TransformAndRefactor(string[] fileContent) {
+            this.fileContent = fileContent;
+        }
+
+        public bool run() {
+            findJSNamespace();
+
+
+            return true;
+        }
+
+        /// <summary>
+        /// Find the namespace of the javascript file.
+        /// </summary>
+        /// <returns>true if a namespace was found.</returns>
+        public bool findJSNamespace() {
+            for (int i = 0; i < fileContent.Length; i++) {
+                if (fileContent[i].Contains("{}")) {
+                    string[] words = fileContent[i].Split(' ');
+
+                    for (int j = 0; j < words.Length; j++) {
+                        if (!words[j].Equals("var")) {
+                            jsNamespace = words[j];
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
