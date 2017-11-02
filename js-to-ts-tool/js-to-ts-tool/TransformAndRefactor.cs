3250c68b727bd5218724f89cbd5e442f6b969cf8
Jimmy Berlin <berlin.jimmy@gmail.com>
2017-11-02 10:54:18 +0100

    js-to-ts-tool hacked together

--- js-to-ts-tool/js-to-ts-tool/TransformAndRefactor.cs
+++ js-to-ts-tool/js-to-ts-tool/TransformAndRefactor.cs
@@ -11,6 +11,12 @@ namespace js_to_ts_tool {
 
         String namespaceRegexPattern = "var [A-Za-z_]+[A-Za-z0-9_-]+[= ]+.*{}";
         String classRegexPattern = ".[A-Za-z0-9_]+ ";
+        String constructorRegexPattern = "= *function *";
+        String endOfFunctionRegexPattern = ".(prototype)?.[A-za-z0-9_][A-za-z0-9_-]* *=* *function *";  //
+        String functionRegexPattern = ".(prototype)?.[A-za-z0-9_][A-za-z0-9_-]*";  //JSC3D.Viewer.(prototype)?.
+        String endOfVariableRegexPattern = @"\.(prototype)?.[A-za-z0-9_][A-za-z0-9_-]* *=* *";  //
+        String variableRegexPattern = ".(prototype)?.[A-za-z0-9_][A-za-z0-9_-]* *= *";
+        String functionJunk = ".(prototype)?.";
 
         String outputPath = "output/";
         
@@ -29,6 +35,8 @@ namespace js_to_ts_tool {
             extractClasses();
             fetchAllClassnames();
             cleanUpClasses();
+            cleanUpFunctions();
+            cleanUpVariables();
             printToFile();
             return true;
         }
@@ -46,9 +54,51 @@ namespace js_to_ts_tool {
                 String path = outputPath + classNames[j] + ".ts";
                 
                 using (StreamWriter sw = File.CreateText(path)) {
-                    sw.Write(classes[i]);
+                    sw.WriteLine(classes[i]);
                     sw.WriteLine(classes[i + 1]);
                     sw.Close();
+                }
+            }
+
+            return true;
+        }
+
+        private bool cleanUpVariables() {
+            int j = 0;
+            for (int i = 2; i < classes.Length; i += 2, j++) {
+                String pattern = jsNamespace + "." + classNames[j] + endOfVariableRegexPattern;
+                MatchCollection wholes = Regex.Matches(classes[i], pattern);
+
+                String variablePattern = jsNamespace + "." + classNames[j] + variableRegexPattern;
+                String junk = jsNamespace + "." + classNames[j] + functionJunk;
+
+                foreach (Match p in wholes) {
+                    String match = p.ToString();
+                    Match fullPath = Regex.Match(match, variablePattern);
+                    String keep = new Regex(junk).Replace(fullPath.ToString(), "");
+
+                    classes[i] = classes[i].Replace(match, keep);
+                }
+            }
+
+            return true;
+        }
+
+        private bool cleanUpFunctions() {
+            int j = 0;
+            for(int i=2;i<classes.Length;i+=2, j++) {
+                String pattern = jsNamespace + "." + classNames[j] + endOfFunctionRegexPattern;
+                MatchCollection wholes = Regex.Matches(classes[i], pattern);
+
+                String functionPattern = jsNamespace + "." + classNames[j] + functionRegexPattern;
+                String junk = jsNamespace + "." + classNames[j] + functionJunk;
+
+                foreach (Match p in wholes) {
+                    String match = p.ToString();
+                    Match fullPath = Regex.Match(match, functionPattern);
+                    String keep = new Regex(junk).Replace(fullPath.ToString(), "");
+
+                    classes[i] = classes[i].Replace(match, keep);
                 }
             }
 
@@ -58,10 +108,18 @@ namespace js_to_ts_tool {
         private bool cleanUpClasses() {
 
             for (int i = 1; i < classes.Length; i+=2) {
+                // Cleanup class definition
                 String whatToReplace = jsNamespace + ".";
                 String replaceWith = "export class ";
                 classes[i] = classes[i].Replace(whatToReplace, replaceWith);
+                classes[i] = classes[i] + " {";
+
+                // Cleanup constructor definition
+                classes[i+1] = new Regex(constructorRegexPattern).Replace(classes[i+1], "constructor", 1);
+
+                classes[i + 1] = classes[i + 1] + "}";
             }
+            
 
             return true;
         }
