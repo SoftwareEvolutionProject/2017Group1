a9115ca6e76e02a3c831aec59a1f1556b16e0347
Jimmy Berlin <berlin.jimmy@gmail.com>
2017-11-07 13:45:01 +0100

    Changing physical-print-edit

--- frontend/web/src/app/components/physical-print/physical-print-edit/physical-print-edit.component.ts
+++ frontend/web/src/app/components/physical-print/physical-print-edit/physical-print-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import {isNullOrUndefined} from 'util';
+import {PhysicalPrintMockService as PhysicalPrintService} from '../../../services/physical-print/physical-print-mock.service';
+import {PhysicalPrint} from '../../../model/physical-print';
+import {ErrorService} from '../../../services/error.service';
 
 @Component({
   selector: 'app-physical-print-edit',
@@ -7,9 +14,118 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhysicalPrintEditComponent implements OnInit {
 
-  constructor() { }
+  @Input('physicalPrint') physicalPrint: PhysicalPrint = null;
+  @Input('nav') nav = true;
+  @Input('creating') creating = false;
+  @Output() changed: EventEmitter<PhysicalPrint> = new EventEmitter<PhysicalPrint>();
+  private loaded = false;
 
-  ngOnInit() {
+  /* forms */
+  private requiredFieldsForm: FormGroup = null;
+
+  constructor(private route: ActivatedRoute,
+              private physicalPrintService: PhysicalPrintService,
+              private formBuilder: FormBuilder,
+              private errorService: ErrorService,
+              private _location: Location) {
   }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const id = params['id'];
+      if (id == 'create') { //new product is being created
+        this.create();
+      }
+      else if (id)
+        this.getData(id);
+    });
+  }
+
+  ngAfterViewInit() {
+  }
+
+  create() {
+    /* init with a boilerplate */
+    this.creating = true;
+    if (this.creating) this.physicalPrint = new PhysicalPrint({});
+    this.populate();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (this.creating) {
+      this.create();
+    } else if (this.physicalPrint) {
+      this.populate();
+    }
+  }
+
+  /* get the product */
+  getData(id) {
+    this.physicalPrintService.get(id).subscribe(
+      (physicalPrint) => {
+        this.physicalPrint = physicalPrint;
+        this.populate();
+      },
+    );
+  }
+
+  /* populate data */
+  private populate() {
+    this.constructForms();
+  }
+
+  /* init forms */
+  private constructForms() {
+    const fields = {
+      name: [this.physicalPrint && this.physicalPrint.name ? this.physicalPrint.name : '',
+        Validators.compose([Validators.required])],
+    };
+
+    this.requiredFieldsForm = this.formBuilder.group(fields);
+    this.loaded = true;
+  }
+
+  /* save product instance */
+  save() {
+    /* convert relevant fields */
+
+    const id = this.physicalPrint.id;
+    const physicalPrint: PhysicalPrint = new PhysicalPrint(this.requiredFieldsForm.value);
+    this.creating ? delete physicalPrint['id'] : physicalPrint.id = id;
+
+    console.log(physicalPrint);
+
+    if (this.creating) { // a new product
+      this.physicalPrint.createCustomer(physicalPrint).subscribe(
+        (data) => {
+          if (this.nav) this.back();
+          this.changed.emit(data);
+        }, (error) => {
+          console.log(error);
+          this.errorService.showAlert(error.verobose_message_header, error.verbose_message);
+        },
+      );
+    } else {
+      this.physicalPrint.updateCustomer(physicalPrint).subscribe(
+        (data) => {
+          if (this.nav) this.back();
+          this.changed.emit(data);
+        }, (error) => {
+          console.log(error);
+          this.errorService.showAlert(error.verobose_message_header, error.verbose_message);
+        },
+      );
+    }
+  }
+
+  cancel() {
+    if (this.nav) this.back();
+    this.changed.emit(this.physicalPrint);
+  }
+
+  back() {
+    this._location.back();
+  }
+
 
 }
