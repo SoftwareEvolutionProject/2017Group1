622c080bef658c9173336cdef2d70d99c607c6a6
Daniel Eineving <daniel@eineving.se>
2017-11-07 12:32:51 +0100

    Added dummy data regarding physical parts and nececities

--- database/fixtures.sql
+++ database/fixtures.sql
@@ -45,15 +45,34 @@ INSERT INTO magicspairing(digitalPrintID, digitalPartID, label) VALUES
 
 INSERT INTO physicalprint (digitalPrintID, slmPath) VALUES
   ((select id from digitalprint where magicsPath = 'path1'), 'slm/path1'),
-  ((select id from digitalprint where magicsPath = 'path1'), 'slm/path2'),
-  ((select id from digitalprint where magicsPath = 'path1'), 'slm/path3');
+  ((select id from digitalprint where magicsPath = 'path1'), 'slm/path2');
 
 INSERT INTO physicalprint (digitalPrintID, slmPath) VALUES
+  ((select id from digitalprint where magicsPath = 'path2'), 'slm/path3'),
   ((select id from digitalprint where magicsPath = 'path2'), 'slm/path4'),
   ((select id from digitalprint where magicsPath = 'path2'), 'slm/path5'),
   ((select id from digitalprint where magicsPath = 'path2'), 'slm/path6'),
-  ((select id from digitalprint where magicsPath = 'path2'), 'slm/path7'),
-  ((select id from digitalprint where magicsPath = 'path2'), 'slm/path8');
+  ((select id from digitalprint where magicsPath = 'path2'), 'slm/path7');
 
+
+
+INSERT INTO orderedpart (orderID, digitalPartID, amount) VALUES
+  ((select id from "order" where date = 2017-10-10), (select id from digitalpart where name = 'Screw'), 40),
+  ((select id from "order" where date = 2017-11-01), (select id from digitalpart where name = 'Hinge'), 10);
+
+INSERT INTO orderedpart (orderID, digitalPartID, amount) VALUES
+  ((select id from "order" where date = 2017-09-09), (select id from digitalpart where name = 'Emil'), 6),
+  ((select id from "order" where date = 2017-09-09), (select id from digitalpart where name = 'Emilia'), 6);
+
+INSERT INTO physicalpart (physicalPrintID, orderedPartID, magicsPartPairingID) VALUES
+  (1,1,1),(1,1,2),(1,1,3),(1,1,4),(1,1,5),(1,2,6),(1,2,7),(1,2,8),
+  (2,1,1),(2,1,2),(2,1,3),(2,1,4),(2,2,6); /*Skipping three on purpose*/
+
+INSERT INTO physicalpart (physicalPrintID, orderedPartID, magicsPartPairingID) VALUES
+  (3,3,9), (3,4,10),
+  (4,3,9), (4,4,10),
+  (5,3,9), (5,4,10),
+  (6,3,9), (6,4,10),
+  (7,3,9), (7,4,10);
 
 COMMIT;
