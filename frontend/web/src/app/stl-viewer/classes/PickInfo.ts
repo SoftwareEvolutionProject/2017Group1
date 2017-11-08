4066162cac8bb5fbc8fe0aee3ebeb4e23329e87d
Mikael Lönn <mikael.lonn@outlook.com>
2017-11-08 12:20:43 +0100

    More lint

--- frontend/web/src/app/stl-viewer/classes/PickInfo.ts
+++ frontend/web/src/app/stl-viewer/classes/PickInfo.ts
@@ -1,24 +1,23 @@
 /**
-	@class PickInfo
-
-	PickInfo is used as the return value of {JSC3D.Viewer}'s pick() method, holding picking result on a given position
-	on the canvas.
+ *   @class PickInfo
+ *   PickInfo is used as the return value of {JSC3D.Viewer}'s pick() method, holding picking result on a given position
+ *   on the canvas.
  */
 export class PickInfo {
-	/**
-	 * {Number} X coordinate on canvas.
-	 */
-	canvasX = 0;
-	/**
-	 * {Number} Y coordinate on canvas.
-	 */
-	canvasY = 0;
-	/**
-	 * {Number} The depth value.
-	 */
-	depth = -Infinity;
-	/**
-	 * {JSC3D.Mesh} Mesh picked on current position or null if none.
-	 */
-	mesh = null;
+    /**
+     * {Number} X coordinate on canvas.
+     */
+    canvasX = 0;
+    /**
+     * {Number} Y coordinate on canvas.
+     */
+    canvasY = 0;
+    /**
+     * {Number} The depth value.
+     */
+    depth = -Infinity;
+    /**
+     * {JSC3D.Mesh} Mesh picked on current position or null if none.
+     */
+    mesh = null;
 }
--- frontend/web/src/app/stl-viewer/classes/PlatformInfo.ts
+++ frontend/web/src/app/stl-viewer/classes/PlatformInfo.ts
@@ -2,25 +2,25 @@
 
 export class PlatformInfo {
 
-    browser =	'other';
-    version =	'n/a';
-    isTouchDevice = false; //(document.createTouch != undefined); 		// detect if it is running on touch device
-    supportTypedArrays = false; //(window.Uint32Array != undefined);			// see if Typed Arrays are supported
-    supportWebGL =	true; //(window.WebGLRenderingContext != undefined);	// see if WebGL context is supported
+    browser = 'other';
+    version = 'n/a';
+    isTouchDevice = false; // (document.createTouch != undefined); // detect if it is running on touch device
+    supportTypedArrays = false; // (window.Uint32Array != undefined); // see if Typed Arrays are supported
+    supportWebGL = true; // (window.WebGLRenderingContext != undefined); // see if WebGL context is supported
 
-	agents = [
-		['firefox', /Firefox[\/\s](\d+(?:\.\d+)*)/],
-		['chrome',  /Chrome[\/\s](\d+(?:\.\d+)*)/ ],
-		['opera',   /Opera[\/\s](\d+(?:\.\d+)*)/],
-		['safari',  /Safari[\/\s](\d+(?:\.\d+)*)/],
-		['webkit',  /AppleWebKit[\/\s](\d+(?:\.\d+)*)/],
-		['ie',      /MSIE[\/\s](\d+(?:\.\d+)*)/],
-		/*
-		 * For IE11 and above, as the old keyword 'MSIE' no longer exists there.
-		 * By Laurent Piroelle <laurent.piroelle@fabzat.com>.
-		 */
-		['ie',      /Trident\/\d+\.\d+;\s.*rv:(\d+(?:\.\d+)*)/],
-	];
+    agents = [
+        ['firefox', /Firefox[\/\s](\d+(?:\.\d+)*)/],
+        ['chrome',  /Chrome[\/\s](\d+(?:\.\d+)*)/ ],
+        ['opera',   /Opera[\/\s](\d+(?:\.\d+)*)/],
+        ['safari',  /Safari[\/\s](\d+(?:\.\d+)*)/],
+        ['webkit',  /AppleWebKit[\/\s](\d+(?:\.\d+)*)/],
+        ['ie',      /MSIE[\/\s](\d+(?:\.\d+)*)/],
+        /*
+         * For IE11 and above, as the old keyword 'MSIE' no longer exists there.
+         * By Laurent Piroelle <laurent.piroelle@fabzat.com>.
+         */
+        ['ie',      /Trident\/\d+\.\d+;\s.*rv:(\d+(?:\.\d+)*)/],
+    ];
 
     constructor() {
         /*var matches = null;
