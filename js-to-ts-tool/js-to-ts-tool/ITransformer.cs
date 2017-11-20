f855d5d3e5e55b6e9af650a43100e29eee37a6b8
Jimmy Berlin <berlin.jimmy@gmail.com>
2017-11-20 13:44:14 +0100

    interface added

---
+++ js-to-ts-tool/js-to-ts-tool/ITransformer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace js_to_ts_tool
+{
+    interface ITransformer
+    {
+        String[] Classes
+        {
+            get;
+        }
+        String[] ClassNames
+        {
+            get;
+        }
+
+        bool FindJSNamespace();
+        bool ExtractClasses();
+        bool FetchAllClassnames();
+        bool CleanUpClasses();
+        bool CleanUpFunctions();
+        bool CleanUpVariables();
+    }
+}
