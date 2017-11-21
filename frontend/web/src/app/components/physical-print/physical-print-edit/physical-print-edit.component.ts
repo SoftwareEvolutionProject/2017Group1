d10fbb1da1d26a99b8aa1c43fdafbb4aeea58548
Mikael Lönn <mikael.lonn@outlook.com>
2017-11-21 20:27:53 +0100

    upload slm file does nothing yet

--- frontend/web/src/app/components/physical-print/physical-print-edit/physical-print-edit.component.ts
+++ frontend/web/src/app/components/physical-print/physical-print-edit/physical-print-edit.component.ts
@@ -1,5 +1,5 @@
 import {Location} from '@angular/common';
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
 import {PhysicalPrint} from '../../../model/physical-print';
@@ -7,6 +7,9 @@ import {ErrorService} from '../../../services/error.service';
 import {PhysicalPrintService} from '../../../services/physical-print/physical-print.service';
 import {DigitalPrintService} from "../../../services/digital-print/digital-print.service";
 import {DigitalPrint} from "../../../model/digital-print";
+import {BsModalRef, BsModalService} from "ngx-bootstrap";
+import {Ng4FilesConfig, Ng4FilesSelected, Ng4FilesService, Ng4FilesStatus} from "angular4-files-upload";
+declare var $: any;
 
 @Component({
   selector: 'app-physical-print-edit',
@@ -20,21 +23,34 @@ export class PhysicalPrintEditComponent implements OnInit, OnChanges {
   @Input('physicalPrint') physicalPrint: PhysicalPrint = null;
   @Input('nav') nav = true;
   @Input('creating') creating = false;
+  private modalRef: BsModalRef;
+  @ViewChild('modalError') modalDelete;
   @Output() changed: EventEmitter<PhysicalPrint> = new EventEmitter<PhysicalPrint>();
   private loaded = false;
+  public selectedFile;
+  public errorMessage;
+  private slmConfig: Ng4FilesConfig = {
+    acceptExtensions: ['.slm'],
+    maxFilesCount: 1,
+    maxFileSize: 5120000,
+    totalFilesSize: 10120000,
+  };
 
   /* forms */
   private requiredFieldsForm: FormGroup = null;
 
-  constructor(private route: ActivatedRoute,
+  constructor(private modalService: BsModalService,
+              private route: ActivatedRoute,
               private physicalPrintService: PhysicalPrintService,
               private digitalPrintService: DigitalPrintService,
               private formBuilder: FormBuilder,
               private errorService: ErrorService,
-              private _location: Location) {
+              private _location: Location,
+              private ng4FilesService: Ng4FilesService) {
   }
 
   ngOnInit(): void {
+    this.ng4FilesService.addConfig(this.slmConfig);
     this.route.params.subscribe((params) => {
       const id = params['id'];
       if (id === 'create') { // new product is being created
@@ -45,6 +61,31 @@ export class PhysicalPrintEditComponent implements OnInit, OnChanges {
     });
   }
 
+  public filesSelect(selectedFiles: Ng4FilesSelected): void {
+    if (selectedFiles.status === Ng4FilesStatus.STATUS_SUCCESS) {
+      this.selectedFile = Array.from(selectedFiles.files).map((file) => file.name)[0];
+    } else if (selectedFiles.status === Ng4FilesStatus.STATUS_MAX_FILE_SIZE_EXCEED) {
+      this.errorMessage = 'Max file size exceeded';
+      this.openModal('#errorFormDismissBtn');
+    } else if (selectedFiles.status === Ng4FilesStatus.STATUS_NOT_MATCH_EXTENSIONS) {
+      this.errorMessage = 'Only ".slm" files allowed!';
+      this.openModal('#errorFormDismissBtn');
+    }
+  }
+
+  private dismissError() {
+    this.modalRef.hide();
+  }
+
+  openModal(autoFocusIdWithHashtag: string) {
+    this.modalRef = this.modalService.show(this.modalDelete);
+    if (autoFocusIdWithHashtag != null && autoFocusIdWithHashtag !== '') {
+      const addInput: any = ($(autoFocusIdWithHashtag) as any);
+      setTimeout(() => {
+        addInput.focus();
+      }, 200);
+    }
+  }
   create() {
     /* init with a boilerplate */
     this.creating = true;
@@ -67,6 +108,7 @@ export class PhysicalPrintEditComponent implements OnInit, OnChanges {
     this.physicalPrintService.getPhysicalPrint(id).subscribe(
       (physicalPrint) => {
         this.physicalPrint = physicalPrint;
+        this.selectedFile = this.physicalPrint.slmPath;
         this.populate();
       },
     );
@@ -102,6 +144,7 @@ export class PhysicalPrintEditComponent implements OnInit, OnChanges {
 
     this.requiredFieldsForm.value.digitalPrintID = parseInt(this.requiredFieldsForm.get('digitalPrintID').value);
     const physicalPrint: PhysicalPrint = new PhysicalPrint(this.requiredFieldsForm.value);
+    physicalPrint.slmPath = this.selectedFile;
     this.creating ? delete physicalPrint['id'] : physicalPrint.id = id;
 
     if (this.creating) { // a new product
