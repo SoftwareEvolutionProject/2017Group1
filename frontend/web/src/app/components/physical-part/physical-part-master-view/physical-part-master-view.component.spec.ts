dc2aee01e064f8e398e1ab49c4dac1200ddb2909
Johan Andersson <lol2kpe@gmail.com>
2017-11-27 14:44:33 +0100

    Add import to physical part master view spec

--- frontend/web/src/app/components/physical-part/physical-part-master-view/physical-part-master-view.component.spec.ts
+++ frontend/web/src/app/components/physical-part/physical-part-master-view/physical-part-master-view.component.spec.ts
@@ -1,13 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HttpModule } from '@angular/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BsModalService, ModalModule } from 'ngx-bootstrap';
 import { PhysicalPartService } from '../../../services/physical-part/physical-part.service';
 import { ErrorService } from '../../../services/error.service';
 import { HttpClientService } from '../../../services/http/http-client.service';
 import { PhysicalPartDetailPanelComponent } from '../physical-part-detail-panel/physical-part-detail-panel.component';
-import { PhysicalPartListComponent}  from '../physical-part-list/physical-part-list.component';
+import { PhysicalPartListComponent} from '../physical-part-list/physical-part-list.component';
 import { PhysicalPartMasterViewComponent } from './physical-part-master-view.component';
 import { PhysicalPartMockService } from '../../../services/physical-part/physical-part-mock.service';
 
