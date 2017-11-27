21401b6ebcad48eef7705cb365dfbb092c9db3a3
Johan Andersson <lol2kpe@gmail.com>
2017-11-27 13:56:51 +0100

    Add use of mock in physical part master view for tests

--- frontend/web/src/app/components/physical-part/physical-part-master-view/physical-part-master-view.component.spec.ts
+++ frontend/web/src/app/components/physical-part/physical-part-master-view/physical-part-master-view.component.spec.ts
@@ -1,14 +1,15 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {HttpModule} from '@angular/http';
-import {RouterTestingModule} from '@angular/router/testing';
-import {ModalModule} from 'ngx-bootstrap';
-import {PhysicalPartService} from '../../../services/physical-part/physical-part.service';
-import {ErrorService} from '../../../services/error.service';
-import {HttpClientService} from '../../../services/http/http-client.service';
-import {PhysicalPartDetailPanelComponent} from '../physical-part-detail-panel/physical-part-detail-panel.component';
-import {PhysicalPartListComponent} from '../physical-part-list/physical-part-list.component';
+import { HttpModule } from '@angular/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ModalModule } from 'ngx-bootstrap';
+import { PhysicalPartService } from '../../../services/physical-part/physical-part.service';
+import { ErrorService } from '../../../services/error.service';
+import { HttpClientService } from '../../../services/http/http-client.service';
+import { PhysicalPartDetailPanelComponent } from '../physical-part-detail-panel/physical-part-detail-panel.component';
+import { PhysicalPartListComponent}  from '../physical-part-list/physical-part-list.component';
 import { PhysicalPartMasterViewComponent } from './physical-part-master-view.component';
+import { PhysicalPartMockService } from '../../../services/physical-part/physical-part-mock.service';
 
 describe('PhysicalPartMasterViewComponent', () => {
   let component: PhysicalPartMasterViewComponent;
@@ -23,7 +24,7 @@ describe('PhysicalPartMasterViewComponent', () => {
     TestBed.overrideComponent(PhysicalPartListComponent, {
       set: {
         providers: [
-          {provide: PhysicalPartService, useClass: PhysicalPartService},
+          {provide: PhysicalPartService, useClass: PhysicalPartMockService},
           {provide: ErrorService, useClass: ErrorService},
         ],
       },
