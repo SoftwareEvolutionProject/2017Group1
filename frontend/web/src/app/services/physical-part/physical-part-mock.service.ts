dfb934bf894a6b726b354f4bbc36bc30a6fa1bb1
Johan Andersson <lol2kpe@gmail.com>
2017-11-27 13:34:22 +0100

    Fix physical part mock service

--- frontend/web/src/app/services/physical-part/physical-part-mock.service.ts
+++ frontend/web/src/app/services/physical-part/physical-part-mock.service.ts
@@ -5,30 +5,37 @@ import {PhysicalPart} from '../../model/physical-part';
 @Injectable()
 export class PhysicalPartMockService {
 
+  public id: number;
+  public orderedPartID: number;
+  public physicalPrintID: number;
+  public magicsPartPairingID: number;
+
   private data: PhysicalPart[] = [
     new PhysicalPart({
       id: 1,
-      orderedPart : 1,
-      physicalPrint : 1,
-      name: 'Cool printed part',
+      physicalPrintID: 1,
+      magicsPartPairingID: 1,
+      orderedPartID : 1,
     }), new PhysicalPart({
       id: 2,
-      orderedPart : 1,
-      physicalPrint : 1,
-      name: 'Best printed part',
+      physicalPrintID: 2,
+      magicsPartPairingID: 4,
+      orderedPartID : 3,
     }),
   ];
 
   constructor() {
   }
 
-  getAll(): Observable<PhysicalPart[]> {
+  getPhysicalParts(): Observable<PhysicalPart[]> {
     return Observable.of(this.data);
   }
-  get(id: number): Observable<PhysicalPart> {
+
+  getPhysicalPart(id: number): Observable<PhysicalPart> {
     return Observable.of( this.data.filter((e) => {if (e.id === id) { return e; } })[0]);
   }
-  delete(id: number) {
+
+  deletePhysicalPart(id: number) {
     this.data = this.data.filter((e) => {if (e.id !== id) { return e; } });
     return Observable.of(true);
   }
