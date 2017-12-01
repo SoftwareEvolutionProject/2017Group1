a22dbad2889145a3743ec564b58d6a204508bb1c
Christopher <christopher.akersten@gmail.com>
2017-12-01 10:55:31 +0100

    changed port

--- frontend/web/src/app/services/http/http.client.ts
+++ frontend/web/src/app/services/http/http.client.ts
@@ -1,5 +1,5 @@
 export class HttpClient {
-  static baseUrl = 'http://localhost:4567/';
+  static baseUrl = 'http://46.101.132.193:4567/';
   static loginUrl = HttpClient.baseUrl + 'login';
   static customerUrl = HttpClient.baseUrl + 'customers';
   static physicalPartUrl = HttpClient.baseUrl + 'physical-parts';
