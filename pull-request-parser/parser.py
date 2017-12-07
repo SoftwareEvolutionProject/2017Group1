2f39c853e13c65a022d04600d62e93893b9cae27
Jimmy Berlin <berlin.jimmy@gmail.com>
2017-12-07 13:51:26 +0100

    Refactoring

--- pull-request-parser/parser.py
+++ pull-request-parser/parser.py
@@ -1,13 +1,67 @@
 import json
 import requests
-import csv
 from requests.auth import HTTPBasicAuth
 from Pullrequest import Pullrequest
-import sys
-import unicodedata
 import collections
-import csv
 
+##############################################################################
+# Pull requests
+##############################################################################
+def get_pull_request(user, password):
+    # Fetching number of requests.
+    r = requests.get('https://api.github.com/repos/SoftwareEvolutionProject/2017Group1/pulls?state=all', auth=(user, password))
+
+    prtext = r.text
+    data = json.loads(prtext)
+
+    pullrequestnr = -1
+    for json_dict in data:
+        for key,value in json_dict.iteritems():
+            if key == "number":
+                if value > pullrequestnr:
+                    pullrequestnr = value
+                
+    print "We found " + str(pullrequestnr) + " pullrequests"
+
+    # Fetching all the pull requests
+    pullrequests = []
+
+    #for i in range(1, pullrequestnr+1):
+    for i in range(1, 7):
+        print "Fetching pullrequest number: " + str(i);
+        r = requests.get('https://api.github.com/repos/SoftwareEvolutionProject/2017Group1/pulls/'+str(i), auth=(user, password))
+        prtext = r.text
+        data = json.loads(prtext)
+
+        pullrequest = Pullrequest()
+
+        for key in data:
+            if key == "url":
+                pullrequest.url = data[key]
+            if key == "number":
+                pullrequest.number = data[key]
+            if key == "title":
+                pullrequest.title = data[key]
+            if key == "user":
+                for userkey, uservalue in data[key].iteritems():
+                    if userkey == "login":
+                        pullrequest.user = uservalue
+                        break
+            if key == "additions":
+                pullrequest.addditions = data[key]
+            if key == "deletions":
+                pullrequest.deletions = data[key]
+            if key == "changed_files":
+                pullrequest.changed_files = data[key]
+
+        pullrequests.append(pullrequest)
+
+    return pullrequests
+
+
+#######################################################################
+# Main
+#######################################################################
 
 # Reading credentials
 file = open("authorization.txt", "r")
@@ -15,53 +69,7 @@ user = file.readline().strip()
 password = file.readline().strip()
 file.close()
 
-# Fetching number of requests.
-r = requests.get('https://api.github.com/repos/SoftwareEvolutionProject/2017Group1/pulls?state=all', auth=(user, password))
-
-prtext = r.text
-data = json.loads(prtext)
-
-pullrequestnr = -1
-for json_dict in data:
-    for key,value in json_dict.iteritems():
-        if key == "number":
-            if value > pullrequestnr:
-                pullrequestnr = value
-            
-print "We found " + str(pullrequestnr) + " pullrequests"
-
-# Fetching all the pull requests
-pullrequests = []
-
-#for i in range(1, pullrequestnr+1):
-for i in range(1, 70):
-    print "Fetching pullrequest number: " + str(i);
-    r = requests.get('https://api.github.com/repos/SoftwareEvolutionProject/2017Group1/pulls/'+str(i), auth=(user, password))
-    prtext = r.text
-    data = json.loads(prtext)
-
-    pullrequest = Pullrequest()
-
-    for key in data:
-        if key == "url":
-            pullrequest.url = data[key]
-        if key == "number":
-            pullrequest.number = data[key]
-        if key == "title":
-            pullrequest.title = data[key]
-        if key == "user":
-            for userkey, uservalue in data[key].iteritems():
-                if userkey == "login":
-                    pullrequest.user = uservalue
-                    break
-        if key == "additions":
-            pullrequest.addditions = data[key]
-        if key == "deletions":
-            pullrequest.deletions = data[key]
-        if key == "changed_files":
-            pullrequest.changed_files = data[key]
-
-    pullrequests.append(pullrequest)
+pullrequests =  get_pull_request(user, password)
 
 # Print data to file
 file = open("output.csv", "w")
