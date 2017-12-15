e084c0fcbc9390b36c1a3bdc19873cf4aed85b98
Christopher <christopher.akersten@gmail.com>
2017-12-15 07:56:03 +0100

    fix

--- frontend/web/src/app/components/stl-viewer/stl-viewer.component.ts
+++ frontend/web/src/app/components/stl-viewer/stl-viewer.component.ts
@@ -26,9 +26,9 @@ export class StlViewerComponent implements OnInit, OnChanges {
 
   ngOnInit() {
     const canvas = document.getElementById('stl');
-    console.log((this.baseUrl + this.stlUrl).replace('//','/'))
+    console.log((this.baseUrl + this.stlUrl).replace("//",'/'))
     const parameters = {
-      SceneUrl: (this.baseUrl + this.stlUrl).replace('//','/'),
+      SceneUrl: (this.baseUrl + this.stlUrl).replace("//",'/'),
     };
 
     // ========================================================================
@@ -43,8 +43,8 @@ export class StlViewerComponent implements OnInit, OnChanges {
   }
   ngOnChanges(changes: SimpleChanges): void {
     if (this.viewer) {
-      console.log((this.baseUrl + this.stlUrl).replace('//','/'))
-      this.viewer.replaceSceneFromUrl((this.baseUrl + this.stlUrl).replace('//','/'));
+      console.log((this.baseUrl + this.stlUrl).replace("//","/"))
+      this.viewer.replaceSceneFromUrl((this.baseUrl + this.stlUrl).replace("//",'/'));
       this.viewer.update();
     }
   }
