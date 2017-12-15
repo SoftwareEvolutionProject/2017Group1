9418b76714345ba58b12e20ef542cf6a32718fd1
Christopher <christopher.akersten@gmail.com>
2017-12-15 07:51:31 +0100

    fix

--- frontend/web/src/app/components/stl-viewer/stl-viewer.component.ts
+++ frontend/web/src/app/components/stl-viewer/stl-viewer.component.ts
@@ -42,7 +42,7 @@ export class StlViewerComponent implements OnInit, OnChanges {
   }
   ngOnChanges(changes: SimpleChanges): void {
     if (this.viewer) {
-      this.viewer.replaceSceneFromUrl(this.baseUrl + this.stlUrl);
+      this.viewer.replaceSceneFromUrl((this.baseUrl + this.stlUrl).replace('//','/'));
       this.viewer.update();
     }
   }
